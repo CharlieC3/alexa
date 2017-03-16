8de21a0d3abb4ab5f286f693dd91635cb8fcbb79
Niranjala <niranjala.senaratne@espn.com>
2017-03-16 14:29:31 -0400

    add getRecording call

--- RadioUp_Show_castaways/js/apiclient.js
+++ RadioUp_Show_castaways/js/apiclient.js
@@ -4,13 +4,31 @@ var servers = [
     'http://api.espn.com'
 ];
 
-
+//Return a list of shows
 function getShows() {
     var showsPath = '/audio/shows?apikey=r7gsmkjvz6b99tspg48swttq';
     var client = resilient({ service: { basePath: '/v1' }})
     client.setServers(servers)
 
     client.get(showsPath, function (err, res) {
+      if (res.status === 200) {
+        console.log('Success:', res.data)
+        return res.data
+      } else {
+        console.log('Error:', err)
+        return err
+      }
+    })
+
+}
+
+//Return a the recording for a given id
+function getRecording(id) {
+    var recordPath = '/audio/shows/id/recordings?apikey=r7gsmkjvz6b99tspg48swttq';
+    var client = resilient({ service: { basePath: '/v1' }})
+    client.setServers(servers)
+
+    client.get(recordPath, function (err, res) {
       if (res.status === 200) {
         console.log('Success:', res.data)
         return res.data
